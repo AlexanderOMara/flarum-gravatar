1a6b3a7a949ed017504dcaf73eb65bd146079226
Alexander O'Mara <me@alexomara.com>
2021-04-04 19:23:14 -0400

    Updated settings

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,10 +2,35 @@ import app from 'flarum/app';
 
 import {ID} from '../config';
 import {intercept} from '../shared/intercept';
-import {GravatarSettingsModal} from './components/GravatarSettingsModal';
 
-app.initializers.add(ID, () => {
+app.initializers.add(ID, app => {
 	intercept();
 
-	app.extensionSettings[ID] = () => app.modal.show(GravatarSettingsModal);
+	app.extensionData.for(ID)
+		.registerSetting({
+			setting: `${ID}.default`,
+			label: (<span>Default (<code>mp</code>, <code>identicon</code>, <code>retro</code>, [URL], ...)</span>),
+			type: 'text'
+		})
+		.registerSetting({
+			setting: `${ID}.default_force`,
+			label: (<span>Force Default Gravatar Icons</span>),
+			type: 'boolean'
+		})
+		.registerSetting({
+			setting: `${ID}.rating`,
+			label: (<span>Rating (<code>g</code>, <code>pg</code>, <code>r</code>, <code>x</code>)</span>),
+			type: 'text'
+		})
+		.registerSetting({
+			setting: `${ID}.disable_local`,
+			label: (<span>Disable Local Avatars</span>),
+			type: 'boolean'
+		})
+		.registerSetting({
+			setting: `${ID}.link_new_tab`,
+			label: (<span>Gravatar Link New Tab</span>),
+			type: 'boolean'
+		})
+	;
 });
